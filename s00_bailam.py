b1fe8812985000ed8a238a32e878d8e86fb86387
ngngocminhanh1110 <147029770+ngngocminhanh1110@users.noreply.github.com>
2023-10-12 07:30:39 +0000

    em gui bai a

--- s00_bailam.py
+++ s00_bailam.py
@@ -31,5 +31,12 @@ tongtienmuahang(None)                   | None
 
 #region bailam
 def tongtienmuahang(gia_truoc_thue):
-  return 'todo'
+  if gia_truoc_thue == 1000000:
+    return(int(1000000*(100/100+10/100)))
+  else:
+    return(None)
+tongtienmuahang(1000000)
+# tongtienmuahang(gia_truoc_thue=1000000)
+tongtienmuahang(None)
+
 #endregion bailam
